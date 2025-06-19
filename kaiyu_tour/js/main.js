5cd84547a51f1670cd88302e7d3224587b8aff8a
瀬川 友仁 <to1mo2.to2mo3@gmail.com>
2025-06-19 23:31:29 +0900

    add:「ツアー紹介」のHTML、CSS実装

--- kaiyu_tour/js/main.js
+++ kaiyu_tour/js/main.js
@@ -8,4 +8,20 @@ $(function(){
         $(".hamburger").removeClass("active");
         $("#header .navi").removeClass("active");
     });
+
+    $("#introduction .tour1 .btn").click(function(){
+        $(this).hide();
+        $("#introduction .tour1 .description").css({
+            "height": "auto",
+            "overflow": "visible"
+        });
+    });
+
+    $("#introduction .tour2 .btn").click(function(){
+        $(this).hide();
+        $("#introduction .tour2 .description").css({
+            "height": "auto",
+            "overflow": "visible"
+        });
+    });
 });
