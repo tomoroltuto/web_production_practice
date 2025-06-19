527dbcddf6420c070c5add27f8cac59380c5c6d5
瀬川 友仁 <to1mo2.to2mo3@gmail.com>
2025-06-20 00:56:38 +0900

    add:「アクティビティ」のHTML、CSS実装

--- kaiyu_tour/js/main.js
+++ kaiyu_tour/js/main.js
@@ -24,4 +24,22 @@ $(function(){
             "overflow": "visible"
         });
     });
+
+    $("#activity .slider").slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 3,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings:{
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
