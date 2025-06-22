89ab7269eb4b5e1be5fc9523d5f3404e2ee40666
瀬川 友仁 <to1mo2.to2mo3@gmail.com>
2025-06-23 00:01:58 +0900

    add:ヘッダーのHTML、CSS実装

---
+++ interior_design/js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+  /*-------------------------------
+  ドロップダウンメニュー
+  ---------------------------------*/
+  $(".navi .menu .menu-first span").click(function(){
+    $(this).toggleClass("active");
+    $(this).next().slideToggle();
+  });
+
+  $(".navi .menu .menu-second").click(function(){
+    $(this).prev().toggleClass("active");
+    $(this).slideToggle();
+  });
+
+  /*-------------------------------
+  ハンバーガーメニュー
+  ---------------------------------*/
+  $(".hamburger").click(function () {
+    $(this).toggleClass("active");
+    $("#header .navi").toggleClass("active");
+    $("#header .mask").toggleClass("avtive");
+  });
+
+  $(".navi a").click(function () {
+    $(".hamburger").removeClass("active");
+    $("#header .navi").removeClass("active");
+    $("#header .mask").removeClass("active");
+  });
+
+  $(".mask").click(function(){
+    $(".hamburger").removeClass("active");
+    $("#header .navi").removeClass("active");
+    $("#header .mask").removeClass("active");
+  });
+
+
+});
